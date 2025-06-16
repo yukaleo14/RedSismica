1aa4c86686ebf1e97da65f5665e7cea69c1fe846
leop <yukaleo43@gmail.com>
2025-06-16 12:27:19 -0300

    Conexion general agregada

--- frontend/EventoSismico-app/vite.config.js
+++ frontend/EventoSismico-app/vite.config.js
@@ -7,4 +7,16 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/autenticacion': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
